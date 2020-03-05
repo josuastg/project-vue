8aa79a5dcb10306be01c51eaaea772e390a2ac65
Josua Pangihutan Sitanggang <josuastg93@gmail.com>
2020-03-05 15:21:11 +0700

    Main app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <agent-table/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AgentTable from '@/components/AgentTable.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AgentTable,
   }
 }
 </script>
